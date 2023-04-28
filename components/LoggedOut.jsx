0e6ea15b619e28c4025548fadb01aeb4830e9a12
James L. Johnson III <jamesljohnson3@gmail.com>
2023-04-27 22:44:49 -0400

    Update LoggedOut.jsx

--- components/LoggedOut.jsx
+++ components/LoggedOut.jsx
@@ -21,7 +21,7 @@ export const LoggedOut = () => {
         <div className="container">
           <div className="card hero">
             <p className="text-display-1 hero-title">
-              Let's start authenticating <br /> with KindeAuth
+              Lets start authenticating <br /> with KindeAuth
             </p>
             <p className="text-body-1 hero-tagline">Configure your app</p>
 
